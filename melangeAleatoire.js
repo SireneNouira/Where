b03a2fe03c9653a4bd9bb668658a0a57047ef29a
Nadir-Bsd <bensadi.nadir42400@gmail.com>
2024-11-29 11:11:12 +0100

    ADD: randomazier

--- melangeAleatoire.js
+++ melangeAleatoire.js
@@ -1,9 +1,30 @@
 // get 
-const n = document.querySelectorAll(".seat");
+// all names
+let names = ["nadir", "sirene", "hamza", "karl", "ali", "jeremi"];
+let count = 0;
+// all places
+const places = document.querySelectorAll(".seat");
+// how much places
+const nPlaces = places.length;
+
 
 // event
 
-// function
-n.forEach((Element) => {
-    console.log(Element.textContent);
+
+// functions
+// randomize array name
+for(let i = nPlaces; i >= 0; i -= 1){
+    // randomizer of j 
+    let j = (Math.floor(Math.random() * nPlaces));
+    // create temp var to containe actual index
+    let temp = names[i];
+    // switch the actual index(i) and randomze index(j)
+    names[i] = names[j];
+    names[j] = temp;
+};
+
+// named place 
+places.forEach((element) => {
+    element.innerHTML = names[count];
+    count += 1;
 });
