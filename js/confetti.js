6a1158a3f6498e422206431dfb6ffb6d501c5296
Nadir-Bsd <bensadi.nadir42400@gmail.com>
2024-12-01 00:29:01 +0100

    ADD: button reveal UPDATE: confetti all code in export function explosion

--- js/confetti.js
+++ js/confetti.js
@@ -2,7 +2,6 @@
 
 // event
 
-
 // const chiffres = document.querySelectorAll("#compteur h1");
 // let index = 0;
 // function afficherChiffres() {
@@ -28,73 +27,77 @@
 
 
 // functions
-const duration = 15 * 1000,
-animationEnd = Date.now() + duration,
-defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
 
-function randomInRange(min, max) {
-  return Math.random() * (max - min) + min;
-}
+export default function explosion() {
 
-const interval = setInterval(function() {
-  const timeLeft = animationEnd - Date.now();
-
-  if (timeLeft <= 0) {
-    return clearInterval(interval);
+  const duration = 15 * 1000,
+  animationEnd = Date.now() + duration,
+  defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+  
+  function randomInRange(min, max) {
+    return Math.random() * (max - min) + min;
   }
-
-  const particleCount = 50 * (timeLeft / duration);
-
-  // since particles fall down, start a bit higher than random
-  confetti(
-    Object.assign({}, defaults, {
-      particleCount,
-      origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
-    })
-  );
-  confetti(
-    Object.assign({}, defaults, {
-      particleCount,
-      origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
-    })
-  );
-}, 250);
-
-const stars = {
-  spread: 360,
-  ticks: 50,
-  gravity: 0,
-  decay: 0.94,
-  startVelocity: 30,
-  shapes: ["star"],
-  colors: ["FFE400", "FFBD00", "E89400", "FFCA6C", "FDFFB8"],
-};
-
-function shootStars() {
-  confetti({
-    ...stars,
-    particleCount: 40,
-    scalar: 1.2,
+  
+  const interval = setInterval(function() {
+    const timeLeft = animationEnd - Date.now();
+  
+    if (timeLeft <= 0) {
+      return clearInterval(interval);
+    }
+  
+    const particleCount = 50 * (timeLeft / duration);
+  
+    // since particles fall down, start a bit higher than random
+    confetti(
+      Object.assign({}, defaults, {
+        particleCount,
+        origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+      })
+    );
+    confetti(
+      Object.assign({}, defaults, {
+        particleCount,
+        origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+      })
+    );
+  }, 250);
+  
+  const stars = {
+    spread: 360,
+    ticks: 50,
+    gravity: 0,
+    decay: 0.94,
+    startVelocity: 30,
     shapes: ["star"],
-  });
-
-  confetti({
-    ...stars,
-    particleCount: 10,
-    scalar: 0.75,
-    shapes: ["circle"],
-  });
+    colors: ["FFE400", "FFBD00", "E89400", "FFCA6C", "FDFFB8"],
+  };
+  
+  function shootStars() {
+    confetti({
+      ...stars,
+      particleCount: 40,
+      scalar: 1.2,
+      shapes: ["star"],
+    });
+  
+    confetti({
+      ...stars,
+      particleCount: 10,
+      scalar: 0.75,
+      shapes: ["circle"],
+    });
+  };
+  
+  setTimeout(shootStars, 0);
+  setTimeout(shootStars, 200);
+  setTimeout(shootStars, 400);
+  setTimeout(shootStars, 600);
+  setTimeout(shootStars, 800);
+  setTimeout(shootStars, 1000);
+  setTimeout(shootStars, 1200);
+  setTimeout(shootStars, 1400);
+  setTimeout(shootStars, 1600);
+  setTimeout(shootStars, 1800);
+  setTimeout(shootStars, 2000);
+  setTimeout(shootStars, 2200);
 };
-
-setTimeout(shootStars, 0);
-setTimeout(shootStars, 200);
-setTimeout(shootStars, 400);
-setTimeout(shootStars, 600);
-setTimeout(shootStars, 800);
-setTimeout(shootStars, 1000);
-setTimeout(shootStars, 1200);
-setTimeout(shootStars, 1400);
-setTimeout(shootStars, 1600);
-setTimeout(shootStars, 1800);
-setTimeout(shootStars, 2000);
-setTimeout(shootStars, 2200);
--- js/reveal.js
+++ js/reveal.js
@@ -1,13 +1,20 @@
+// IMPORT
+// import the function explosion from confetti.js
+import explosion from "./confetti.js";
+
 // GET 
+// button for reveal
+const button = document.querySelector("button");
 // get all div with class .seat
 const divNames = document.querySelectorAll(".seat");
 var time = 1000;
 // EVENT 
-window.addEventListener("load", appear);
+button.addEventListener("click", reveal)
 
 // FUNCTION
 
-function appear(event) {
+function reveal(event) {
+    explosion();
     divNames.forEach(divName => {
         setTimeout(() => {
             divName.classList.add("appear");
