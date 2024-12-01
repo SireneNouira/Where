333508e8bfa5bf5e1950bfbc2d081c2fb9b2dd3e
SireneNouira <sirene.nouira16@gmail.com>
2024-12-01 14:21:27 +0100

    prenom sauvegarder en local fait sur accueil js !

--- js/accueil.js
+++ js/accueil.js
@@ -1,16 +1,39 @@
-//GET
-// liste d'inputs name et bouton generé
 const genere = document.querySelector(".button-genere > a");
 const inputs = document.querySelectorAll("input");
+const saveButton = document.getElementById("saveNames");
 var noms = [];
 
-// event
-genere.addEventListener("click", handleAddNoms);
 
-// function
-function handleAddNoms() {
+window.addEventListener("DOMContentLoaded", loadNames);
+
+
+saveButton.addEventListener("click", saveNames);
+
+
+function saveNames() {
+  noms = [];
   inputs.forEach((input) => {
-    noms.push(input.value);
+    if (input.value.trim() !== "") {
+      noms.push(input.value.trim());
+    }
   });
+
+  if (noms.length === 15) {
+    localStorage.setItem("savedNames", JSON.stringify(noms));
+    alert("Prénoms sauvegardés !");
+  } else {
+    alert("Veuillez remplir les 15 prénoms.");
+  }
+}
+
+
+function loadNames() {
+  const savedNames = JSON.parse(localStorage.getItem("savedNames"));
+  if (savedNames && savedNames.length === 15) {
+    inputs.forEach((input, index) => {
+      input.value = savedNames[index] || "";
+    });
+    alert("Prénoms chargés depuis la dernière sauvegarde !");
+  }
 }
 
--- js/confetti.js
+++ js/confetti.js
@@ -24,8 +24,6 @@
 //   }
 // }
 // afficherChiffres();
-
-
 // functions
 
 export default function explosion() {
