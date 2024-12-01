a642b4c60a67fdedb72b07f8ddfcd2f389db4533
Nadir-Bsd <bensadi.nadir42400@gmail.com>
2024-12-01 18:21:16 +0100

    ADD: menuBurger

---
+++ js/burgerMenu.js
@@ -0,0 +1,28 @@
+// GET 
+// get menu burger button
+const buttonBurger = document.querySelector(".buttonBurger");
+// get button create menu burger
+const buttonCreate = document.querySelector(".create");
+// get input menu burger
+const input = document.querySelector("input");
+
+console.log(buttonCreate);
+console.log(input);
+
+
+// EVENT
+buttonBurger.addEventListener("click", apearBurgerMenu);
+buttonCreate.addEventListener("click", createDiv);
+
+
+// FUNCTION
+
+// make apear burger menu
+function apearBurgerMenu(event) {
+};
+
+// create element in container 
+function createDiv(event) {
+    
+};
+
